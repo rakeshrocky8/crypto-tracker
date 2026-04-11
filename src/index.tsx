64801a37d1cdddc1c748fc47b62b23d2e32ac6a4
rakeshrocky8 <rockytfg@gmail.com>
2026-04-11 11:05:15 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 
 const root = ReactDOM.createRoot(
@@ -15,4 +15,4 @@ root.render(
 );
 
 
-reportWebVitals(console.log);
+
